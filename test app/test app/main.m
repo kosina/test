34fc9b3d9fd1e695f0acad1cbf65c9c4c878e34e
Marek Kosinski <kosina@kosina.pl>
2012-07-29 21:39:39 +0200

    version 1

---
+++ test app/test app/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test app
+//
+//  Created by Marek Kosinski on 29.07.2012.
+//  Copyright (c) 2012 Marek Kosinski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
